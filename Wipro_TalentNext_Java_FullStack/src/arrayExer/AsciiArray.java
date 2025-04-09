53ad615ef6d234f2d8154c6537799a118d275038
Smitanshu Ukey <smitanshuukey3070@gmail.com>
2025-04-09 23:55:20 +0530

    Array

--- Wipro_TalentNext_Java_FullStack/src/arrayExer/AsciiArray.java
+++ Wipro_TalentNext_Java_FullStack/src/arrayExer/AsciiArray.java
@@ -1,12 +1,12 @@
 package arrayExer;
+
 public class AsciiArray {
-	public static void main(String[] args) {
-	char []array= new char[]{65,66,67,68,69,70,97,98,99,100,101,102};	
-	for(int i=0; i<array.length;i++) {
-		System.out.print(array[i]+" ");
-	         }
-	
-	
-   }
+    public static void main(String[] args) {
+        char [] array = new char []{65, 66, 67, 68, 69, 70, 97, 98, 99, 100, 101, 102};
+        for (char c : array) {
+            System.out.print(c + " ");
+        }
+
+    }
 
 }
